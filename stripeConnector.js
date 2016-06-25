248e9e53f54a2bdf587c9a80ff0b14089354d50a
Akos Kemives <akoskm@gmail.com>
2016-06-25 16:34:53 +0200

    add stripe integration module

---
+++ stripeConnector.js
@@ -0,0 +1,40 @@
+var stripe = require('stripe')(
+  'sk_test_beutyrG1DRWiK5SKC0A8H6q6'
+);
+
+module.exports = {
+
+  createOrder: function (req, cb) {
+    var items = req.items,
+        email = req.email;
+
+    // Always validate the user input before handling it to an external API.
+    // If you can detect requests that would fail anyway, you'll spare the
+    // cost of the network request before it's even made.
+    if (items === null || items.length < 1) {
+      cb('No items found.', null);
+    }
+
+    // a terrible way to validate an email address
+    if (!email) {
+      cb('Email is required.', null);
+    }
+
+    stripe.orders.create({
+      currency: 'usd',
+      items: items,
+      email: email
+    }, function(err, order) {
+      // stripe response, asynchronously called
+      if (err) {
+        cb(err, null);
+      } else {
+        if (!order || !order.id) {
+          cb('Unknown error occurred.', null);
+        } else {
+          cb(null, order);
+        }
+      }
+    });
+  }
+};
